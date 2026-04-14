9d80d02d213bca5c633c502a938c9ae9ca24247e
Brian Jepson <bjepson@gmail.com>
2026-04-14 15:09:06 -0400

    modularised

--- ch03/ch3-pms5003.py
+++ ch03/ch3-pms5003.py
@@ -17,12 +17,13 @@ read_command.extend(start_of_frame)
 read_command.extend(command_read)
 read_command.extend(sum(read_command).to_bytes(2, "big"))
 
-timeout = 200 # in ms
-uart = UART(0,9600, tx=Pin(12), rx=Pin(13))
 
-uart.write(set_passive_command)
+def init_pms5003(uart):
+    uart.write(set_passive_command)
 
-def read_pms5003():
+
+def read_pms5003(uart):
+    timeout = 200 # in ms
     uart.write(read_command)
     
     time.sleep(0.1)
@@ -30,7 +31,7 @@ def read_pms5003():
     raw_data = bytes()
     got_data = False
     while uart.any() > 0:
-        #need to loop through to avoid timeout
+        # need to loop through to avoid timeout
         last_data = time.ticks_ms()
         while time.ticks_ms() < last_data + timeout:
             if uart.any():
@@ -42,15 +43,19 @@ def read_pms5003():
             data = struct.unpack(data_format, raw_data)
             return (data[6],data[7],data[8])
         except ValueError:
-            return(-1,-1,-1)
+            return(-1, -1, -1)
             print("data malformed")
     
-    return(-1,-1,-1)
+    return(-1, -1, -1)
 
-print("listenning")
-while True:
-    time.sleep(5)
-    pm1, pm25, pm10 = read_pms5003()
-    print("pm1: %d ug/m^3"% pm1)
-    print("pm2.5: %d ug/m^3"% pm25)
-    print("pm10: %d ug/m^3"% pm10)
+
+if __name__ == "__main__":
+    uart = UART(0, 9600, tx=Pin(12), rx=Pin(13))
+    init_pms5003(uart)
+    
+    while True:
+        time.sleep(5)
+        pm1, pm25, pm10 = read_pms5003(uart)
+        print("pm1: %d ug/m^3"% pm1)
+        print("pm2.5: %d ug/m^3"% pm25)
+        print("pm10: %d ug/m^3"% pm10)
