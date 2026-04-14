78e7ca1c767d71e20f5ea039c548f2a2647ef49f
Brian Jepson <bjepson@gmail.com>
2026-04-14 13:23:02 -0400

    Repo had wrong code in it (ch03.docx was correct)

--- ch03/ch3-bme280-spi.py
+++ ch03/ch3-bme280-spi.py
@@ -6,12 +6,11 @@ import digitalio
 from adafruit_bme280 import basic as adafruit_bme280
 
 spi = busio.SPI(board.GP2, MISO=board.GP4, MOSI=board.GP3)
-cs = digitalio.DigitalInOut(board.GP0)
+cs = digitalio.DigitalInOut(board.GP5)
 bme280 = adafruit_bme280.Adafruit_BME280_SPI(spi, cs)
 
 while True:
-    print("\nTemperature: %0.1f C" % bme280.temperature)
+    print("Temperature: %0.1f C" % bme280.temperature)
     print("Humidity: %0.1f %%" % bme280.humidity)
     print("Pressure: %0.1f hPa" % bme280.pressure)
     time.sleep(1)
-
