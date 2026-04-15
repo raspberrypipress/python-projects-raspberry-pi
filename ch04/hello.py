1e9be82d708fa79602bd0aaf4921ff4c2905ed53
Brian Jepson <bjepson@gmail.com>
2026-04-15 15:27:26 -0400

    copied over code from the book

--- ch04/hello.py
+++ ch04/hello.py
@@ -1,27 +1,23 @@
-# Example file showing a circle moving on screen
 import pygame
 import pygame.freetype
 
 pygame.init()
-
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 running = True
 dt = 0
-font = pygame.freetype.Font("/home/ben/pybook/ch4-pygame/OpenSans-Regular.ttf", 24)
+font = pygame.freetype.Font(
+    "/path/to/OpenSans-Regular.ttf", 24)
 
 while running:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    #RENDER
+    # RENDER
     screen.fill("purple")
-    
     text_surface, rect = font.render("Hello World!", (0, 0, 0))
     screen.blit(text_surface, (40, 250))
-
     pygame.display.flip()
 
     dt = clock.tick(60) / 1000
