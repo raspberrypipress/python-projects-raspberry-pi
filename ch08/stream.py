1adfb6a7ceccbf8647c85c20fb338085b258c813
Brian Jepson <bjepson@gmail.com>
2026-04-20 15:55:40 -0400

    Only taking one reading per loop instead of one per axis

--- ch08/stream.py
+++ ch08/stream.py
@@ -2,7 +2,6 @@ from time import sleep, ticks_ms
 import board
 import busio
 import adafruit_lsm303_accel
-import adafruit_lis2mdl
 import machine
 
 button = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_UP)
@@ -11,11 +10,12 @@ i2c = busio.I2C(board.GP17, board.GP16)
 accel = adafruit_lsm303_accel.LSM303_Accel(i2c)
 
 while True:
+    reading = accel.acceleration
     print(
         ticks_ms(),
-        accel.acceleration[0],
-        accel.acceleration[1],
-        accel.acceleration[2],
+        reading[0],
+        reading[1],
+        reading[2],
         button.value(), 
         sep=",")
     sleep(0.1)
