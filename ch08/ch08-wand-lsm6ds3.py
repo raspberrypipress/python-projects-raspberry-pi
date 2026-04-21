7f53d1faf1c7d995cf020218146ceda099efbfeb
Brian Jepson <bjepson@gmail.com>
2026-04-21 07:10:12 -0400

    Wand with support for lsm6ds3

---
+++ ch08/ch08-wand-lsm6ds3.py
@@ -0,0 +1,53 @@
+import emlearn_trees
+import array
+import gc
+import _thread
+from time import sleep, ticks_ms
+import machine
+from lsm6ds3 import LSM6DS3, NORMAL_MODE_104HZ # https://github.com/pimoroni/lsm6ds3-micropythonv
+import neopixel
+#from led_helpers import hsv_to_rgb, sparkle
+
+run_sparkle = False
+#np = neopixel.NeoPixel(machine.Pin(2), 10)
+
+def core1_loop():
+    global run_sparkle, np
+    while True:
+        while not run_sparkle:
+            sleep(0.1)
+        #sparkle(np,10,7,3,5,0.9)
+        print("sparkle")
+        run_sparkle = False
+
+_thread.start_new_thread(core1_loop, ())
+
+i2c = machine.I2C(0, sda=machine.Pin(16), scl=machine.Pin(17))
+sensor = LSM6DS3(i2c, mode=NORMAL_MODE_104HZ, address=0x6b)
+
+print("loading model")
+model = emlearn_trees.new(300, 10000, 200)
+
+with open('flick_model.csv', 'r') as f:
+    emlearn_trees.load_model(model, f)
+
+resout = array.array('f',[0,0])
+
+window  =[]
+for i in range(10):
+    window.append(array.array('h',[0]*30))
+
+print("running")
+while True:
+    for i in range(10):
+        reading = sensor.get_readings()
+        for j in range(10):
+            window[i][(i+j)%10] = int(reading[0]*1000)
+            window[i][(i+j)%10+10] = int(reading[1]*1000)
+            window[i][(i+j)%10+20] = int(reading[2]*1000)
+        sleep(0.1)
+        model.predict(window[i], resout)
+        if(resout[1] > 0.73):
+            print("flick detected at ", ticks_ms())
+            run_sparkle = True
+
