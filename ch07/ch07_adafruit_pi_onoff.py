2fc938be8a91ee542f5c4c3e2f3d737eec870e4a
Brian Jepson <bjepson@gmail.com>
2026-04-22 09:14:47 -0400

    running->collecting

--- ch07/ch07_adafruit_pi_onoff.py
+++ ch07/ch07_adafruit_pi_onoff.py
@@ -5,7 +5,7 @@ import time
 from Adafruit_IO import MQTTClient
 
 from adafruit_bme280 import basic as adafruit_bme280
-running = True
+collecting = True
 
 ADAFRUIT_AIO_USERNAME = "xyz"
 ADAFRUIT_AIO_KEY      = "XYZ"
@@ -13,13 +13,13 @@ ADAFRUIT_AIO_KEY      = "XYZ"
 aio = MQTTClient(ADAFRUIT_AIO_USERNAME, ADAFRUIT_AIO_KEY)
 
 def message(client, feed_id, payload):
-    global running
+    global collecting
     print('Feed {0} received new value: {1}'.format(
           feed_id, payload))
     if payload == 'ON':
-        running = True
+        collecting = True
     if payload == 'OFF':
-        running = False
+        collecting = False
     
 aio.on_message = message
 
@@ -35,7 +35,8 @@ bme280 = adafruit_bme280.Adafruit_BME280_SPI(spi, cs)
 counter = 0
 while True:
     if counter == 5:
-        if running:
+        print(collecting)
+        if collecting:
             print("\nTemperature: %0.1f C" % bme280.temperature)
             print("Humidity: %0.1f %%" % bme280.humidity)
             aio.publish("temp", bme280.temperature)
