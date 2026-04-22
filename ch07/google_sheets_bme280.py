24930edebd455da338394bd04ca3a59820edbce7
Brian Jepson <bjepson@gmail.com>
2026-04-22 08:09:00 -0400

    copied code from book

--- ch07/google_sheets_bme280.py
+++ ch07/google_sheets_bme280.py
@@ -20,5 +20,6 @@ bme280 = adafruit_bme280.Adafruit_BME280_SPI(spi, cs)
 while True:
     print("\nTemperature: %0.1f C" % bme280.temperature)
     print("Humidity: %0.1f %%" % bme280.humidity)
-    worksheet.append_row([str(datetime.datetime.now()),bme280.temperature,bme280.humidity])
+    worksheet.append_row([str(datetime.datetime.now()),
+        bme280.temperature,bme280.humidity])
     time.sleep(1)
