6e1546079b60dd8138973a57dcad2d509855d651
Brian Jepson <brian.jepson@raspberrypi.com>
2026-04-24 15:34:00 -0400

    whitespace

--- ch04/weather.py
+++ ch04/weather.py
@@ -14,7 +14,7 @@ url = "https://api.open-meteo.com/v1/forecast"
 params = {
     "latitude": 52.52,
     "longitude": 13.41,
-    "daily": ["temperature_2m_max", 
+    "daily": ["temperature_2m_max",
               "precipitation_probability_max"],
     "forecast_days": 3
 }
